be16d0d7dd7d34554908e311d1ed414578fdad7e
Francois Lebel <francoislebel@gmail.com>
2015-06-04 22:04:13 -0700

    Improved lookup of DOM modifications.

--- travis-cat.js
+++ travis-cat.js
@@ -2,17 +2,25 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-var containerExists = setInterval(function() {
+MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+var observer = new MutationObserver(function(mutations, observer) {
+  comfortingCat = document.getElementById("comforting-cat");
   logContainer = document.getElementById("log-container");
-  if (logContainer) {
+  if (!comfortingCat && logContainer) {
     status_errored = document.getElementsByClassName("icon icon-status errored")[0];
     status_failed = document.getElementsByClassName("icon icon-status failed")[0];
     if (status_errored != undefined || status_failed != undefined) {
       imageTag = document.createElement("img");
-      imageTag.src = "http://thecatapi.com/api/images/get?format=src&type=gif&size=med";
+      imageTag.setAttribute("id", "comforting-cat");
+      imageTag.setAttribute("src", "http://thecatapi.com/api/images/get?format=src&type=gif&size=med");
       logContainer.parentNode.insertBefore(imageTag, logContainer);
     }
-    clearInterval(containerExists);
   }
-}, 500);
+});
+
+observer.observe(document, {
+  childList: true,
+  subtree: true
+});
 
